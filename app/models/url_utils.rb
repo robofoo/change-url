0b546df9a6df0abc696c7acde305e93478e5be3a
Daniel <robotright@gmail.com>
2012-08-24 19:06:33 +0800

    remove url utils - forgot we need to access active record

--- app/models/url_utils.rb
+++
@@ -1,7 +0,0 @@
-module UrlUtils
-  extend self
-
-  def shorten(url)
-    'utils shortened it'
-  end
-end
--- spec/models/url_utils_spec.rb
+++
@@ -1,7 +0,0 @@
-require './app/models/url_utils'
-
-describe UrlUtils do
-  it 'works' do
-    pending
-  end
-end
