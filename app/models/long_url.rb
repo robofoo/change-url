cf9b4ad6fe45bf159795d1c68b514d2493a351dc
Daniel <robotright@gmail.com>
2012-08-24 17:57:58 +0800

    add simple validation

--- app/models/long_url.rb
+++ app/models/long_url.rb
@@ -1,4 +1,5 @@
 class LongUrl < ActiveRecord::Base
   has_many :short_urls
   attr_accessible :url
+  validates :url, :presence => true
 end
--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,4 +1,5 @@
 class ShortUrl < ActiveRecord::Base
   belongs_to :long_url
   attr_accessible :url
+  validates :url, :presence => true
 end
