aae149881a838b0f28b85a194be8fe406e1b3de6
Daniel <robotright@gmail.com>
2012-08-25 15:11:04 +0800

    add urls to database

--- app/models/long_url.rb
+++ app/models/long_url.rb
@@ -10,8 +10,8 @@ class LongUrl < ActiveRecord::Base
   # verify not a bad word
   # verify unique
   # create variations
-  def self.shorten(url)
-    new_url = ''
+  def self.shorten(long_url)
+    short_url = ''
     begin
       checker = Checker.new(
                   UrlChecker.new(
@@ -20,11 +20,32 @@ class LongUrl < ActiveRecord::Base
                     )
                   )
                 )
-      new_url = checker.verify_url(url)
+      short_url = checker.verify_url(long_url)
+      # LongUrl handles it since it references short_urls
+      LongUrl.add_urls(long_url, short_url)
     rescue => exception
       raise exception
     end
 
-    new_url
+    short_url
+  end
+
+  def self.add_urls(long_url, short_url)
+    ref_url = LongUrl.new(url:long_url)
+    ref_url.save!
+
+    # add similar urls
+    similar = [short_url]
+    similar << short_url.gsub('l', '1') if short_url.include?('l')
+    similar << short_url.gsub('1', 'l') if short_url.include?('1')
+    similar << short_url.gsub('o', '0') if short_url.include?('o')
+    similar << short_url.gsub('0', 'o') if short_url.include?('0')
+
+    similar.each do |sim|
+      ShortUrl.new(url:sim).tap do |surl|
+        surl.long_url = ref_url
+        surl.save!
+      end
+    end
   end
 end
